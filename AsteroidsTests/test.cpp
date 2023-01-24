19c46ca7ce39503a3c9128d4ab35e7da787c8a21
Ken Power <github@kenpower.com>
2023-01-24 17:33:50 +0000

    More collision tests

--- AsteroidsTests/test.cpp
+++ AsteroidsTests/test.cpp
@@ -6,10 +6,43 @@ TEST(TestCaseName, TestName) {
   EXPECT_TRUE(true);
 }
 
-TEST(Collision, SameEntityControllerCollidesWithIteslf) {
+TEST(Collision, SameEntityControllerCollidesWithItself) {
 	
 	EntityController *e = new EntityController;
 	e->settings(0, 0);
 	
 	EXPECT_TRUE(isCollide(e, e));
+}
+
+TEST(Collision, OvelappingEntityControllersCollide) {
+
+	EntityController* e = new EntityController;
+	e->settings(0, 0);
+	EntityController* f = new EntityController;
+	f->settings(0.5, 0);
+
+	EXPECT_TRUE(isCollide(e, f));	
+	EXPECT_TRUE(isCollide(f, e));
+}
+
+TEST(Collision, SeperatedEntityControllersDoNotCollide) {
+
+	EntityController* e = new EntityController;
+	e->settings(0, 0);
+	EntityController* f = new EntityController;
+	f->settings(2.5, 0);
+
+	EXPECT_FALSE(isCollide(e, f));	
+	EXPECT_FALSE(isCollide(f, e));
+}
+
+TEST(Collision, EntityControllerInsideAnotherCollide) {
+
+	EntityController* e = new EntityController;
+	e->settings(0, 0, 0, 100);
+	EntityController* f = new EntityController;
+	f->settings(2.5, 0);
+
+	EXPECT_TRUE(isCollide(e, f));
+	EXPECT_TRUE(isCollide(f, e));
 }
